e83ec0db755c52dd12aa8fea72a188a104a7ceed
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2022-05-22 21:03:54 -0500

    Set Values Modal feat
    Created a modal that will appear after selecting the "Añadir valores" button

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useState } from 'react'
-import { View, TextInput, Text, Button } from 'react-native'
-
+import { StyleSheet, View, TextInput, Text, Button, Modal } from 'react-native'
 
 const StartScreen = ({onSolve}) => {
     const [processes, setProcesses] = useState(1)
     const [quatntum, setQuatntum] = useState(1)
+    const [modalVisible, setModalVisible] = useState(false);
     
     const onChangeProcesses = (numProc) => {
         if(!numProc){
@@ -22,25 +22,61 @@ const StartScreen = ({onSolve}) => {
 
     return (
     <View>
-        
-        <Text>Ingrese la cantidad de procesos: </Text>
-        <TextInput
-            placeholder='Cantidad de Procesos'
-            onChangeText={newText => onChangeProcesses(newText)}
-        />
+        <View>
+            <Text>Ingrese la cantidad de procesos: </Text>
+            <TextInput
+                placeholder='Cantidad de Procesos'
+                onChangeText={newText => onChangeProcesses(newText)}
+            />
 
-        <Text>Ingrese el valor del quantum en ticks: </Text>
-        <TextInput
-            placeholder='Quantum en ticks'
-            onChangeText={q => onChangeQuantum(q)}
-        />
+            <Text>Ingrese el valor del quantum en ticks: </Text>
+            <TextInput
+                placeholder='Quantum en ticks'
+                onChangeText={q => onChangeQuantum(q)}
+            />
+        </View>
+        
+
+        <Modal
+            transparent={false}
+            visible={modalVisible}
+            animationType={'slide'}
+        >
+            <View style={styles.modalContainer}>
+                <TextInput
+                    placeholder='Tiempo de entrada del proceso'
+                />
+                <TextInput
+                    placeholder='Duración del proceso'
+                />
+            </View>
+            <Button
+                title='Editar Valores'
+                onPress={ () => setModalVisible(false) }
+            />
+            <Button
+                title='Resolver'
+                onPress={ () => onSolve(processes, quatntum, 1) }
+            />
+        </Modal>
 
         <Button
-            title='Resolver'
-            onPress={ () => onSolve(processes, quatntum, 1) }
+            title='Añadir valores'
+            onPress={() => setModalVisible(!modalVisible)}
         />
+
     </View>
   )
 }
 
 export default StartScreen
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        height: '50%',
+        width: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+});
