590fa7a8aeeff818ea32d58b3623b4f5f5a41523
GranOla18 <80288848+GranOla18@users.noreply.github.com>
2022-05-23 01:29:18 -0500

    "lenght" -> duration feat, added comments, deleted extra condition

--- logic.js
+++ logic.js
@@ -1,34 +1,43 @@
 class Process {
-    constructor(name, length, starts) {
+    constructor(name, duration, starts) {
         this.name = name;
-        this.length = length;
+        this.duration = duration;
         this.starts = starts;
         this.result=[];
     }
 }
 
-const lengths=[7, 3, 9, 8, 10, 12, 6, 5];
+//0 = Wait
+//1 = Being processed
+//2 = N/A
+
+const durations=[7, 3, 9, 8, 10, 12, 6, 5];
 const startTimes=[0, 2, 4, 12, 16, 22, 25, 28];
 let amount=8;
 
 const processArray=[];
 
 for(var i=0;i<amount;i++){
-    processArray.push(new Process(i, lengths[i], startTimes[i]));
+    processArray.push(new Process(i, durations[i], startTimes[i]));
 }
 
-
 let time=0;
+
+//Tick per quantum
 let pass=0;
 let quantum=5;
 
+//Processes in the waiting line
 const line=[];
 
+//Ended processes
 const ended=[];
 
+//This occurs every tick
 do{
     console.log("Current time: ", time);
 
+    //If a process has been executed for a quantum and there are elements in the waiting line
     if ((pass==quantum) && (line.length>0)){
         let temporal=line[0];
         line.shift();
@@ -52,20 +61,22 @@ do{
     }
     console.log("Salgo");
 
+    //If there are processes in the waiting line
     if(line.length>0){
-        line[0].length--;
+        line[0].duration--;
         line[0].result.push(1);
         pass++;
-    
-        if (line.length>0){
-            line.forEach(element => {
-                element.result.push(0);
-            });
-            line[0].result.pop();
-        }
+        
+        //Add waiting tick for each waiting process
+        line.forEach(element => {
+            element.result.push(0);
+        });
+        line[0].result.pop();
 
-        if(line[0].length==0){
+        //If process[0] is done
+        if(line[0].duration==0){
             ended.push(line[0]);
+            //Delete from the array the element[0]
             line.shift();
             pass=0;
         }
@@ -73,6 +84,7 @@ do{
         if(processArray.length>0){
             processArray.forEach(element => {
                 if (element.starts>time){
+                    //It hasn't started
                     element.result.push(2);
                 }
             });
